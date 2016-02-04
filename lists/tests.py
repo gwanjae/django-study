7e2eb0f30ed531e43c22e52d4ed2dda0cb9cc8d2
gjyoo <gjyoo@credif.co.kr>
2016-02-04 16:13:53 +0900

    STATIC_ROOT 설정과 관리자 사이트 무효화

--- lists/tests.py
+++ lists/tests.py
@@ -6,25 +6,72 @@ from django.test import TestCase
 from lists.models import Item, List
 from lists.views import home_page
 
-# Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
+    def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text':'신규 작업 아이템'}
-        )
+        expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
-        #self.assertTrue(response.content.startswith(b'<html'))
-        #self.assertIn(b'<title>To-Do lists</title>', response.content)
-        #self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data = {'item_text' : '신규 작업 아이템'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data = {'item_text' : '신규 작업 아이템'}
+        )
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+
+
+class NewItemTest(TestCase):
+    
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data = {'item_text' : '기존 목록에 신규 아이템'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data = {'item_text' : '기존 목록에 신규 아이템'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
 
 class ListViewTest(TestCase):
 
@@ -32,6 +79,14 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
+
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -48,62 +103,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, '다른 목록 아이템 1')
         self.assertNotContains(response, '다른 목록 아이템 2')
 
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get('/lists/%d/' % (correct_list.id,))
-        self.assertEqual(response.context['list'], correct_list)
-
-class NewItemTet(TestCase):
-    
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data = {
-               'item_text' : '기존 목록에 신규 아이템'
-            }
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data = {
-               'item_text' : '기존 목록에 신규 아이템'
-            }
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
-
-
-class NewListTest(TestCase):
-
-    def test_saving_a_POST_request(self):
-        self.client.post(
-            '/lists/new',
-            data = {'item_text' : '신규 작업 아이템'}
-        )
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '신규 작업 아이템')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new',
-            data = {'item_text' : '신규 작업 아이템'}
-        )
-        new_list = List.objects.first()
-        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 
 class ListAndItemModelsTest(TestCase):
@@ -113,12 +112,12 @@ class ListAndItemModelsTest(TestCase):
         list_.save()
 
         first_item = Item()
-        first_item.text = '첫번째 아이템'
+        first_item.text = '첫 번째 아이템'
         first_item.list = list_
         first_item.save()
 
         second_item = Item()
-        second_item.text = '두번째 아이템'
+        second_item.text = '두 번째 아이템'
         second_item.list = list_
         second_item.save()
 
@@ -130,7 +129,8 @@ class ListAndItemModelsTest(TestCase):
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫번째 아이템')
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
         self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
         self.assertEqual(second_saved_item.list, list_)
+
