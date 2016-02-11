da522bd951708f47e04af34629bed7bc37a4ea64
gjyoo <gjyoo@credif.co.kr>
2016-02-11 17:54:14 +0900

    list view를 리펙터링해서 신규 아이템 POST를 처리하도록함

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -48,43 +48,13 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
+        expected_error = escape("빈 아이템을 등록할 수 없습니다")
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-
-
-
-class NewItemTest(TestCase):
-    
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data = {'item_text' : '기존 목록에 신규 아이템'}
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
-        self.assertEqual(new_item.list, correct_list)
-
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data = {'item_text' : '기존 목록에 신규 아이템'}
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 
 
@@ -117,3 +87,29 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, '다른 목록 아이템 1')
         self.assertNotContains(response, '다른 목록 아이템 2')
+
+    
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data = {'item_text' : '기존 목록에 신규 아이템'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id,),
+            data = {'item_text' : '기존 목록에 신규 아이템'}
+        )
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from lists.models import Item, List
 from django.utils.html import escape
 
 def home_page(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
 
 def new_list(request):
@@ -14,18 +14,16 @@ def new_list(request):
         item.full_clean()
         item.save()
     except ValidationError:
-    	list_.delete()
-        error = escape("You can't have an empty list item")
+        list_.delete()
+        error = escape("빈 아이템을 등록할 수 없습니다")
         return render(request, 'home.html', {'error': error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
-
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	return render(request, 'list.html', {'list': list_})
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+       Item.objects.create(text=request.POST['item_text'], list=list_)
+       print(list_.id,)
+       return redirect('/lists/%d/' % (list_.id,))
+    return render(request, 'list.html', {'list': list_})
